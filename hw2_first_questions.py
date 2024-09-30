e6cae76b6ba670937e40cd632f3890157fdfc7b3
Maria Aleman <mariaaleman@ciutadella-133-106.eduroamcc.upf.edu>
2024-09-30 13:27:42 +0200

    first_questions

---
+++ hw2_first_questions.py
@@ -0,0 +1,20 @@
+### HW2 First Questions 
+
+import pandas as pd
+
+## Question 1
+def triple(x):
+    x = x*3
+    return(x)
+
+
+## Question 2
+def subtract(x,y):
+    z = y - x
+    return(z)
+
+
+## Question 3
+def dictionary_maker(t):
+    dict = {key:value for key, value in t}
+    return(dict)
